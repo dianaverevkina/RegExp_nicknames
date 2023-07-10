12e744c2de31a4bc08557dcc4dde5f6699551242
Diana Verevkina <diana_verevkina@mail.ru>
2023-07-10 18:52:19 +0300

    update function validateUsername

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,8 +1,13 @@
 export default class Validator {
   static validateUsername(username) {
-    const regexp = /^[^\d_-][\w-]*[^\d_-]$/gi;
-    const numberRegExp = /\d{4,}/g;
+    const latinCharsNumsDashUnderscoreAccept = /[\w-]/gi;
+    const exeptThreeNumbersInRow = /\d{4,}/;
+    const startExceptNumDashUnderScore = /^[^\d_-]/i;
+    const EndExceptNumDashUnderScore = /[^\d_-]$/i;
 
-    return regexp.test(username) && !numberRegExp.test(username);
+    return latinCharsNumsDashUnderscoreAccept.test(username)
+      && !exeptThreeNumbersInRow.test(username)
+      && startExceptNumDashUnderScore.test(username)
+      && EndExceptNumDashUnderScore.test(username);
   }
 }
